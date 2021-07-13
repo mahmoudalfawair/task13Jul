58d8881346f588275070044851c4b22b704ce9c6
mahmoudalfawair <84728207+mahmoudalfawair@users.noreply.github.com>
2021-07-13 12:08:50 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n ;
+    cin >> n ;
+    if(n>0)
+        cout << "The number is positive \n";
+    else if(n=0)
+        cout << " The number equals to 0 \n";
+    else
+        cout << "The number is negative \n";
+    return 0;
+}
