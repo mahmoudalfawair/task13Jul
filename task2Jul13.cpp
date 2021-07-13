9ac47da5a2037242d3574773d25c7ce7adf8885e
mahmoudalfawair <84728207+mahmoudalfawair@users.noreply.github.com>
2021-07-13 12:17:19 +0300

    Add files via upload

---
+++ task2Jul13.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int n = 18;
+    int yourAge ;
+    cin >> yourAge ;
+    if(yourAge > n )
+    cout << "Yes , you can vote \n";
+return 0;     
+}
